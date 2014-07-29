7ff51d6c5fc428baceb7d7d497b06b2df382f85c
a00721293 <andytung215@hotmail.com>
2014-07-29 04:19:00 -0700

    added write to text file class

---
+++ src/ca/bcit/comp2613/restaurant/Report.java
@@ -0,0 +1,40 @@
+package ca.bcit.comp2613.restaurant;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import ca.bcit.comp2613.restaurant.model.Employee;
+import ca.bcit.comp2613.restaurant.util.StaffManagement;
+
+public class Report
+{
+
+	public static void main(String[] args) 
+	{
+		PropertyConfigurator.configure(TestDriver.class.getResourceAsStream("log4j.properties")	);
+		Logger log = Logger.getLogger(Report.class);
+		try
+		{
+			log.info("Writing to text file....");
+			ArrayList<Employee> employee = StaffManagement.createEmployee();
+			FileUtils.writeLines(new File("employee.txt"), employee );
+			log.info(FileUtils.readFileToString(new File("employee.txt") ));
+		}
+		catch(IOException ex)
+		{
+			ex.printStackTrace();
+			
+		}
+		
+	}
+	
+	
+
+	
+
+}
