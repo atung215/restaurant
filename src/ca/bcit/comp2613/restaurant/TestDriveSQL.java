f9e4d4b9889e759788b4c2498900b5a6110c38d1
a00721293 <andytung215@hotmail.com>
2014-07-30 07:23:49 -0700

    db

---
+++ src/ca/bcit/comp2613/restaurant/TestDriveSQL.java
@@ -0,0 +1,16 @@
+package ca.bcit.comp2613.restaurant;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
+
+
+@EnableAutoConfiguration
+@ImportResource("applicationContext.xml")
+
+public class TestDriveSQL
+{
+
+	
+}
+
+
